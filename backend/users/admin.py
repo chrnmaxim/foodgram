dbbb095c7cf1080b81b1bb5a7950ef1d4e4f6faf
chrnmaxim <chrn.maxim@gmail.com>
2024-08-02 15:49:01 +0300

    refactor: Переопределена регистрации модели в админ-панели с помощью декоратора `@admin.register`

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from users.models import User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     """Определяет отображение пользователей в панели администратора."""
 
@@ -17,5 +18,3 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ('username', 'email')
     empty_value_display = '-пусто-'
 
-
-admin.site.register(User, UserAdmin)
