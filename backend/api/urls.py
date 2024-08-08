50bcbab4bb94f9f3641d311e2ed61a5c2b2a8bb6
chrnmaxim <chrn.maxim@gmail.com>
2024-08-08 15:09:21 +0300

    dev: Добавлен эндпоинт `users`

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,17 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from users.views import CustomUsersViewSet
+
+app_name = 'api'
+
+router_v1 = DefaultRouter()
+
+router_v1.register('users', CustomUsersViewSet, basename='users')
 
 urlpatterns = [
+    path('', include(router_v1.urls)),
     path('', include('swagger.routes')),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
