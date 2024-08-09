a771d12681d5474bd254f440429773d6da469c36
chrnmaxim <chrn.maxim@gmail.com>
2024-08-09 13:34:35 +0300

    dev: Удалена модель `TagInRecipe`

--- backend/tags/models.py
+++ backend/tags/models.py
@@ -23,26 +23,3 @@ class Tag(models.Model):
         """Определяет отображение наименования тега в админ-панели."""
 
         return f'{self.name}_{self.slug}'[: settings.ADMIN_CHARS_LIMIT]
-
-
-class TagInRecipe(models.Model):
-    """Модель тегов в рецептах."""
-
-    tag = models.ForeignKey(
-        Tag,
-        null=True,
-        on_delete=models.SET_NULL,
-        related_name='tags_in_recipe',
-    )
-    recipe = models.ForeignKey(
-        'recipes.Recipe',
-        on_delete=models.CASCADE,
-        related_name='tags_in_recipe',
-    )
-
-    def __str__(self):
-        """Определяет отображение тегов в рецептах в админ-панели."""
-
-        return f'{self.recipe.name}_{self.recipe.name}'[
-            : settings.ADMIN_CHARS_LIMIT
-        ]
