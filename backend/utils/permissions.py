98f0a8544f8dc0f56b76c12989a49c499d927e1b
chrnmaxim <chrn.maxim@gmail.com>
2024-08-10 19:20:41 +0300

    dev: Обновлены `RecipePermissions`

--- backend/utils/permissions.py
+++ backend/utils/permissions.py
@@ -9,27 +9,46 @@ class CustomPermissions(BasePermission):
     Устанавливается на уровне проекта.
     """
 
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-        ) or obj.author == request.user
-
     def has_permission(self, request, view):
-        if request.user.is_anonymous and (
-            request.method in ['list', 'retrieve', 'GET']
-        ):
+        if request.method in ['list', 'retrieve', 'GET']:
             return 'rest_framework.permissions.AllowAny'
 
 
 class RecipePermissions(BasePermission):
     """Ограничение для эндпоинтов `recipes`."""
 
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-        ) or obj.author == request.user
-
-    def has_permission(self, request, view):
-        return not request.user.is_anonymous or (
-            request.method in permissions.SAFE_METHODS
+            or request.user.is_admin
+            or request.user.is_moderator
+            or obj.author == request.user
         )
+
+
+
+# class IsAdminOrOwnerOrReadOnly(permissions.BasePermission):
+#     """
+#     Разрешены только безопасные методы,
+#     а также доступ суперпользователю или автору объекта.
+#     """
+
+#     def has_permission(self, request, view):
+#         return (
+#             request.method in permissions.SAFE_METHODS
+#             or request.user.is_authenticated
+#         )
+
+#     def has_object_permission(self, request, view, obj):
+#         return (
+#             request.method in permissions.SAFE_METHODS
+#             or request.user.is_admin
+#             or request.user.is_moderator
+#             or obj.author == request.user
+#         )
